c224bbb2c0286de7804d39fd4892139bfc90c78d
Chun Yue Leung <aryue123321@gmail.com>
2019-01-10 05:12:00 -0800

    Add project files.

---
+++ Pepper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pepper.Startup))]
+namespace Pepper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
